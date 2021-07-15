2d33790fd1046130b51341240daa7ceff83e4788
Gael Porteu de La Morandière <gael.porteu@gmail.com>
2021-07-15 11:44:26 -0400

    Add functionality of drop down menu on index.html page

--- js/index.js
+++ js/index.js
@@ -1,14 +1,16 @@
 const randomFolks = document.querySelector(".random-peeps");
+const selectUserNumber = document.querySelector("select")
 
-const getData = async () => {
-    const usersRequest = await fetch('https://randomuser.me/api?results=5');
+// console.log(selectUserNumber); // test to see if the selectUserNumber is working
+
+const getData = async (numUsers) => {
+    const usersRequest = await fetch(`https://randomuser.me/api?results=${numUsers}`);
     const data = await usersRequest.json();
     const usersResults = data.results;
     // console.log(usersResults)
     displayUsers(usersResults)
 }
 
-getData()
 
 const displayUsers = usersResults => {
     randomFolks.innerHTML = "";
@@ -16,15 +18,21 @@ const displayUsers = usersResults => {
         const country = user.location.country;
         const name = user.name.first;
         const imageUrl = user.picture.medium;
-       
-
+        
+        
         const userDiv = document.createElement("div");
         userDiv.innerHTML = `
-            <h3>${name}</h3>
-            <p>${country}</p>
-            <img src=${imageUrl} alt="${name} avatar" />
+        <h3>${name}</h3>
+        <p>${country}</p>
+        <img src=${imageUrl} alt="${name} avatar" />
         `;
-
+        
         randomFolks.appendChild(userDiv)
     }
 }
+
+selectUserNumber.addEventListener("change", event => {
+    const numUsers = event.target.value;
+    // console.log(numUsers); // test
+    getData(numUsers)
+})
